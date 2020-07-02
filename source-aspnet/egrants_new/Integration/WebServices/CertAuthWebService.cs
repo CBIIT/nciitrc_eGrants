59e0fb4012a623a6e238ec83480d6f9094a16a71
Shell <shellba@nih.gov>
2020-07-02 00:30:25 -0400

    Initial push development of web services to regular repository.

---
+++ source-aspnet/egrants_new/Integration/WebServices/CertAuthWebService.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics.Eventing.Reader;
+using System.EnterpriseServices;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Runtime.Remoting.Messaging;
+using System.Security.Cryptography.X509Certificates;
+using System.Web;
+using egrants_new.Integration.Models;
+
+namespace egrants_new.Integration.WebServices
+{
+    public class CertAuthWebService : BaseWebService
+    {
+
+        public CertAuthWebService(WebServiceEndPoint ws):base( ws)
+        {
+            base.WebService.AuthenticationType = Enumerations.AuthenticationType.Certificate;
+        }
+
+        public override void AddAuthentication(ref HttpWebRequest webRequest)
+        {
+            if (WebService.AuthenticationType == Enumerations.AuthenticationType.Certificate)
+            {
+                if (File.Exists(WebService.CertificatePath))
+                {
+                    try
+                    {
+                        X509Certificate2 certificate = new X509Certificate2(WebService.CertificatePath, WebService.CertificatePwd);
+                        webRequest.ClientCertificates.Add(certificate);
+                    } 
+                    catch
+                    {
+                        throw new Exception("Adding Certificate Failed");
+                    }
+
+                }
+                else
+                {
+                    throw new FileNotFoundException("The Specified Client Certificate for Authentication could not be found");
+                }
+            }
+            else
+            {
+                throw new Exception("Something when wrong.  Authentication not set.");
+            }
+        }
+    }
+
+}
+
+
---
+++ source-aspnet/egrants_new/Integration/WebServices/IAuthCertificate.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace egrants_new.Integration.WebServices
+{
+    interface IAuthCertificate
+    {
+        string CertificatePath { get; set; }
+        string CertificatePwd { get; set; }
+    }
+}
