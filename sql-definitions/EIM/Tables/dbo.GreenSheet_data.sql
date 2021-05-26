ef2dd315d44ac4aec0bbcaf895a6be6559d19726
Shell <shellba@nih.gov>
2021-05-26 13:15:39 -0400

    eGrants 14 and eGrants 52

--- sql-definitions/EIM/Tables/dbo.GreenSheet_data.sql
+++ sql-definitions/EIM/Tables/dbo.GreenSheet_data.sql
@@ -12,7 +12,9 @@ CREATE TABLE [dbo].[GreenSheet_data](
 	[GPMATS_CLOSED_FLAG] [varchar](1) COLLATE SQL_Latin1_General_Pref_CP1_CI_AS NULL,
 	[DUMMY_FLAG] [varchar](1) COLLATE SQL_Latin1_General_Pref_CP1_CI_AS NULL,
 	[MULTIYEAR_AWARD_FLAG] [varchar](1) COLLATE SQL_Latin1_General_Pref_CP1_CI_AS NULL,
-	[REVISION_TYPE_CODE] [varchar](20) COLLATE SQL_Latin1_General_Pref_CP1_CI_AS NULL
+	[REVISION_TYPE_CODE] [varchar](20) COLLATE SQL_Latin1_General_Pref_CP1_CI_AS NULL,
+	[CURRENT_ACTION_STATUS_DESC] [varchar](50) COLLATE SQL_Latin1_General_Pref_CP1_CI_AS NULL,
+	[SUFFIX_CODE] [varchar](6) COLLATE SQL_Latin1_General_Pref_CP1_CI_AS NULL
 ) ON [PRIMARY]
 
 GO
