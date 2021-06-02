d73e0ec1949502a7d4b044fd1bab91708261cf18
Shell <shellba@nih.gov>
2021-06-02 12:23:26 -0400

    Updated SQL To support SQL Clone down.

--- sql-definitions/EIM/UserDefinedFunctions/dbo.fn_get_doc_url.sql
+++ sql-definitions/EIM/UserDefinedFunctions/dbo.fn_get_doc_url.sql
@@ -1,9 +1,9 @@
 ﻿SET ANSI_NULLS ON
-SET QUOTED_IDENTIFIER ON
+SET QUOTED_IDENTIFIER OFF
 
 
 
-CREATE   FUNCTION [dbo].[fn_get_doc_url] (@document_id int, @IC varchar(5))
+CREATE       FUNCTION [dbo].[fn_get_doc_url] (@document_id int, @IC varchar(5))
 
 RETURNS varchar(800) AS 
 
@@ -21,11 +21,49 @@ declare
 @fsr_id					int,
 @nga_rpt_seq_num		int,
 @appl_id				int,
-@nga_id					int
+@nga_id					int,
+@server_name   varchar(255),
+@s2server_name varchar(255),
+@sql_report_server varchar(255)
 
+
+
+set @server_name = @@SERVERNAME
 set @IC=LOWER(@IC)
+
+if @server_name = 'NCIDB-D387-V\MSSQLEGRANTSD'  --'NCIDB-Q356-V\MSSQLEGRANTSQ', 'NCIDB-P232-V\MSSQLEGRANTSP' ,'NCIDB-S222-V\MSSQLEGRANTSS'
+BEGIN
 set @locall_image_server='https://egrants-web-dev.'+@IC+'.nih.gov/'
 set @impac_image_server='https://i2e-dev.'+@IC+'.nih.gov/'
+set @s2server_name = 'https://s2s.era.nih.gov/'
+set @sql_report_server = 'https://ncidb-d387-v.nci.nih.gov/ReportServer_MSSQLEGRANTSD/'
+END
+
+IF @server_name = 'NCIDB-Q356-V\MSSQLEGRANTSQ'
+BEGIN
+set @locall_image_server='https://egrants-web-test.'+@IC+'.nih.gov/'
+set @impac_image_server='https://i2e-test.'+@IC+'.nih.gov/'
+set @s2server_name = 'https://s2s.era.nih.gov/'
+set @sql_report_server = 'https://ncidb-q356-v.nci.nih.gov/ReportServer_MSSQLEGRANTSQ/'
+END
+
+IF @server_name = 'NCIDB-S222-V\MSSQLEGRANTSS'
+BEGIN
+set @locall_image_server='https://egrants-web-stage.'+@IC+'.nih.gov/'
+set @impac_image_server='https://i2e-stage.'+@IC+'.nih.gov/'
+set @s2server_name = 'https://s2s.era.nih.gov/'
+set @sql_report_server = 'https://ncidb-s222-v.nci.nih.gov/ReportServer_MSSQLEGRANTSS/'
+
+END
+
+IF @server_name = 'NCIDB-P232-V\MSSQLEGRANTSP'
+BEGIN
+set @locall_image_server='https://egrants.'+@IC+'.nih.gov/'
+set @impac_image_server='https://i2e.'+@IC+'.nih.gov/'
+set @s2server_name = 'https://s2s.era.nih.gov/'
+set @sql_report_server = 'https://ncidb-p232-v.nci.nih.gov/ReportServer_MSSQLEGRANTSP/'
+
+END
 
 SELECT 
 @url=[url],
@@ -42,29 +80,35 @@ WHERE document_id=@document_id
 IF @url is not null 
 BEGIN
 	IF (substring(@url,1,5)='data/' or substring(@url,1,5)='/data') SET @doc_url= @locall_image_server + @url 
-	IF (substring(@url,1,23)='https://s2s.era.nih.gov') SET @doc_url = @url
+	IF @url like @s2server_name +'%' SET @doc_url = @url
 	IF (select PATINDEX('%documentviewer%',@url))>0 SET @doc_url = @url
 END
 
 IF @url is null
 BEGIN
 
-IF (@impac_doc_type='ENG' and @created_date>'04/10/2007' and @IC='nci') SET @doc_url='https://s2s.era.nih.gov/docservice/dataservices/document/once/keyId/' + CONVERT(varchar,@nga_rpt_seq_num) + '/' + @impac_doc_type
+IF (@impac_doc_type='ENG' and @created_date>'04/10/2007' and @IC='nci') SET @doc_url=@s2server_name+'docservice/dataservices/document/once/keyId/' + CONVERT(varchar,@nga_rpt_seq_num) + '/' + @impac_doc_type
 
 --IF (@impac_doc_type='ENG' and @created_date<'04/10/2007') SET @doc_url='https://businessobjects-sg-dev.nci.nih.gov/BOE/OpenDocument/1211012223/CrystalReports/viewrpt.cwr?apspassword=egrants1234&id=30939&apsuser=egrants&apsauthtype=secEnterprise&prompt0='+convert(varchar,@nga_id)+'&promptOnRefresh=1&wid=9b60a3fc9d06464e'
-IF (@impac_doc_type='ENG' and @created_date<'04/10/2007') SET @doc_url='https://ncidb-d201-v.nci.nih.gov/ReportServer_MSSQLEGRANTSP/Pages/ReportViewer.aspx?%2fNGAReports%2fgetNGAfromNGAid&rs:Command=Render&ngaid='+convert(varchar,@nga_id)
+IF (@impac_doc_type='ENG' and @created_date<'04/10/2007') SET @doc_url= @sql_report_server+'Pages/ReportViewer.aspx?%2fNGAReports%2fgetNGAfromNGAid&rs:Command=Render&ngaid='+convert(varchar,@nga_id)
 
 ----PRAM Documents,MYP Documents,Final Invention Statement eAddition
-IF @impac_doc_type in('FSR','AWS','PRM','MYP','CLD','WBR','PRACPC','PRANCE','PRACOV') SET @doc_url='https://s2s.era.nih.gov/docservice/dataservices/document/once/keyId/' + CONVERT(varchar,@nga_rpt_seq_num) + '/' + @impac_doc_type
+IF @impac_doc_type in('FSR','AWS','PRM','MYP','CLD','WBR','PRACPC','PRANCE','PRACOV') SET @doc_url=@s2server_name + 'docservice/dataservices/document/once/keyId/' + CONVERT(varchar,@nga_rpt_seq_num) + '/' + @impac_doc_type
 
 ----Application File,NOA,Final Invention Statement,FPA Documents,FRAM Closeout Documents,Progress Final,JIT, PRAM Documents,Summary Statement,
-IF @impac_doc_type in ('IGI','FIS','FPA','FRM','FPR','JIT','MPR','SS','FRPPR','IRPPR','IPA') SET @doc_url='https://s2s.era.nih.gov/docservice/dataservices/document/once/applId/' + convert(varchar,@appl_id) + '/' + @impac_doc_type
+IF @impac_doc_type in ('IGI','FIS','FPA','FRM','FPR','JIT','MPR','SS','FRPPR','IRPPR','IPA') SET @doc_url=@s2server_name + 'docservice/dataservices/document/once/applId/' + convert(varchar,@appl_id) + '/' + @impac_doc_type
 
 ---Greensheet PGM, Greensheet Spec,Greensheet DMC
 IF @impac_doc_type in('DM','PGM','SPEC') SET @doc_url=@impac_image_server+'greensheets/retrievegreensheet.do?GS_GROUP_TYPE='+ @impac_doc_type +'&EXTERNAL=TRUE&APPL_ID=' + CONVERT(varchar, @appl_id)+ '&GRANT_ID='+@full_grant_num ---COLLATE DATABASE_DEFAULT 
 
+----NEW PRAM
+IF @impac_doc_type in('PRM_NEW') SET @doc_url=@s2server_name + 'docservice/dataservices/document/once/keyId/' + CONVERT(varchar,@nga_rpt_seq_num) + '/CORAM'
+
 ---Greensheet Rev
 IF @impac_doc_type in('REV') SET @doc_url=@impac_image_server+'greensheets/retrievegreensheet.do?GS_GROUP_TYPE='+ @impac_doc_type +'&AGT_ID=' + CONVERT(varchar, @nga_rpt_seq_num)+ '&ORACLE_ID=NCIGAB'
+
+---Program Revision Greensheet 
+IF @impac_doc_type in('PGMREV') SET @doc_url=@impac_image_server+'greensheets/retrievegreensheet.do?GS_GROUP_TYPE='+ @impac_doc_type +'&AGT_ID=' + CONVERT(varchar, @nga_rpt_seq_num)+ '&ORACLE_ID=NCIGAB'
 
 ---Greensheet ARC
 IF @impac_doc_type in('ARC') SET @doc_url=@impac_image_server+'greensheets/retrievegreensheet.do?GS_GROUP_TYPE='+ @impac_doc_type +'&AGT_ID=' + CONVERT(varchar, @nga_rpt_seq_num)
@@ -73,7 +117,7 @@ IF @impac_doc_type in('ARC') SET @doc_url=@impac_image_server+'greensheets/retri
 IF @impac_doc_type='FSR'
 BEGIN
 	select @fsr_id=(select max(fsr_id) from impp_fsrs_all where appl_id=@appl_id) ---else set @fsr_id=null
-	set @doc_url='https://s2s.era.nih.gov/docservice/dataservices/document/once/keyId/'+convert(varchar,@fsr_id) +'/'+ @impac_doc_type
+	set @doc_url=@s2server_name + 'docservice/dataservices/document/once/keyId/'+convert(varchar,@fsr_id) +'/'+ @impac_doc_type
 END
 
 END
