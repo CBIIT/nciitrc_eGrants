adc10f76ceb67538d2c57d2a7fac507a6b45af22
Shell <shellba@nih.gov>
2021-09-13 14:47:55 -0400

    Filtered out null grant id

--- source-aspnet/egrants_new/Management/Models/QCAssignment.cs
+++ source-aspnet/egrants_new/Management/Models/QCAssignment.cs
@@ -80,6 +80,7 @@ namespace egrants_new.Models
             and disabled_date is null
             and ic = @ic
             AND parent_id IS null
+            and grant_id is not null 
             group by qc_person_id)
 
             Select qc.files_to_qc, qc.qc_days,
