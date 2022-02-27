74c119b2897f98c36d620e2078508f1f6b48ff31
benny shell <xosippa@gmail.com>
2022-02-27 15:20:08 -0500

    Refactor of the Institutional Files

--- source-aspnet/egrants_new/Egrants/DataRepositories/InstitutionalFilesRepo.cs
+++ source-aspnet/egrants_new/Egrants/DataRepositories/InstitutionalFilesRepo.cs
@@ -10,56 +10,48 @@ using egrants_new.Models;
 
 namespace egrants_new.Egrants.Models
 {
-    public class InstitutionalFiles
+    public class InstitutionalFilesRepo
     {
-        public class OrgNameIndex
+        private System.Data.SqlClient.SqlConnection conn;
+       // private System.Data.SqlClient.SqlCommand cmd;
+
+        public InstitutionalFilesRepo()
         {
-            public string index_id { get; set; }
-            public string character_index { get; set; }
-            public string index_seq { get; set; }
+            conn = new System.Data.SqlClient.SqlConnection(ConfigurationManager.ConnectionStrings["EgrantsDB"].ConnectionString);
         }
 
+
+
         //to load Character index for intitution name 
-        public static List<OrgNameIndex> LoadOrgNameCharacterIndex()
+        public List<InsitutionalOrgNameIndex> LoadOrgNameCharacterIndices()
         {
-            System.Data.SqlClient.SqlConnection conn = new System.Data.SqlClient.SqlConnection(ConfigurationManager.ConnectionStrings["EgrantsDB"].ConnectionString);
             System.Data.SqlClient.SqlCommand cmd = new System.Data.SqlClient.SqlCommand("SELECT index_id, character_index, index_seq from dbo.character_index order by index_seq", conn);
             cmd.CommandType = CommandType.Text;
             conn.Open();
 
-            var OrgNameCharacterIndexs = new List<OrgNameIndex>();
+            var OrgNameCharacterIndexs = new List<InsitutionalOrgNameIndex>();
             SqlDataReader rdr = cmd.ExecuteReader();
             while (rdr.Read())
             {
-                OrgNameCharacterIndexs.Add(new OrgNameIndex
+                OrgNameCharacterIndexs.Add(new InsitutionalOrgNameIndex()
                 {
-                    index_id = rdr["index_id"]?.ToString(),
-                    character_index = rdr["character_index"]?.ToString(),
-                    index_seq = rdr["index_seq"]?.ToString()
+                    IndexId = rdr["index_id"]?.ToString(),
+                    CharacterIndex = rdr["character_index"]?.ToString(),
+                    IndexSeq = rdr["index_seq"]?.ToString()
                 });
             }
             conn.Close();
             return OrgNameCharacterIndexs;
         }
 
-        public class OrgFiles
-        {
-            public string tag { get; set; }
-            public string org_id { get; set; }
-            public string org_name { get; set; }
-            public string created_by { get; set; }
-            public string created_date { get; set; }
-            public string end_date { get; set; }
-            public string sv_url { get; set; }
-        }
-
         //to load all intitution list
-        public static List<OrgFiles> LoadOrgList(string act, string str, int index_id, int org_id, int doc_id, int category_id, string file_type, string start_date, string end_date, string ic, string userid)
+        public List<InstitutionalOrg> LoadOrgList(InstitutionalFilesPageAction action, string str, int index_id, int org_id, int doc_id, int category_id, string file_type, string start_date, string end_date, string ic, string userid)
         {
-            System.Data.SqlClient.SqlConnection conn = new System.Data.SqlClient.SqlConnection(ConfigurationManager.ConnectionStrings["EgrantsDB"].ConnectionString);
+
             System.Data.SqlClient.SqlCommand cmd = new System.Data.SqlClient.SqlCommand("sp_web_egrants_institutional_files", conn);
             cmd.CommandType = CommandType.StoredProcedure;
-            cmd.Parameters.Add("@act", System.Data.SqlDbType.VarChar).Value = act;
+            //TODO:  This should branched to different Stored procedures based on the revision MAdhu does
+            //cmd.Parameters.Add("@act", System.Data.SqlDbType.VarChar).Value = act;
             cmd.Parameters.Add("@str", System.Data.SqlDbType.VarChar).Value = str;
             cmd.Parameters.Add("@index_id", System.Data.SqlDbType.Int).Value = index_id;
             cmd.Parameters.Add("@org_id", System.Data.SqlDbType.Int).Value = org_id;
@@ -73,15 +65,14 @@ namespace egrants_new.Egrants.Models
 
             conn.Open();
 
-            var OrgFilesList = new List<OrgFiles>();
+            var OrgList = new List<InstitutionalOrg>();
             SqlDataReader rdr = cmd.ExecuteReader();
             while (rdr.Read())
             {
-                OrgFilesList.Add(new OrgFiles
+                OrgList.Add(new InstitutionalOrg()
                 {
-                    tag = rdr["tag"]?.ToString(),
-                    org_id = rdr["org_id"]?.ToString(),
-                    org_name = rdr["org_name"]?.ToString(),
+                    OrgId = (int)rdr["org_id"],
+                    Org_name = rdr["org_name"]?.ToString(),
                     created_by = rdr["created_by"]?.ToString(),
                     created_date= rdr["created_date"]?.ToString(),
                     end_date = rdr["end_date"]?.ToString(),
@@ -89,26 +80,12 @@ namespace egrants_new.Egrants.Models
                 });
             }
             conn.Close();
-            return OrgFilesList;
-        }
-
-        public class DocFiles
-        {
-            public string tag { get; set; }
-            public string org_id { get; set; }
-            public string org_name { get; set; }
-            public string document_id { get; set; }
-            public string category_name { get; set; }
-            public string url { get; set; }
-            public string start_date { get; set; }
-            public string end_date { get; set; }
-            public string created_date { get; set; }
+            return OrgList;
         }
 
         //to load all intitutional files list
-        public static List<DocFiles> LoadOrgDocList(string act, string str, int index_id, int org_id, int doc_id, int category_id, string file_type, string start_date, string end_date, string ic, string userid)
+        public List<InstitutionalDocFiles> LoadOrgDocList(string act, string str, int index_id, int org_id, int doc_id, int category_id, string file_type, string start_date, string end_date, string ic, string userid)
         {
-            System.Data.SqlClient.SqlConnection conn = new System.Data.SqlClient.SqlConnection(ConfigurationManager.ConnectionStrings["EgrantsDB"].ConnectionString);
             System.Data.SqlClient.SqlCommand cmd = new System.Data.SqlClient.SqlCommand("sp_web_egrants_institutional_files", conn);
             cmd.CommandType = CommandType.StoredProcedure;
             cmd.Parameters.Add("@act", System.Data.SqlDbType.VarChar).Value = act;
@@ -125,13 +102,12 @@ namespace egrants_new.Egrants.Models
 
             conn.Open();
 
-            var DocFilesList = new List<DocFiles>();
+            var DocFilesList = new List<InstitutionalDocFiles>();
             SqlDataReader rdr = cmd.ExecuteReader();
             while (rdr.Read())
             {
-                DocFilesList.Add(new DocFiles
+                DocFilesList.Add(new InstitutionalDocFiles
                 {
-                    tag = rdr["tag"]?.ToString(),
                     org_id = rdr["org_id"]?.ToString(),
                     org_name = rdr["org_name"]?.ToString(),
                     document_id = rdr["document_id"]?.ToString(),
@@ -146,30 +122,21 @@ namespace egrants_new.Egrants.Models
             return DocFilesList;
         }
 
-        public class OrgCategory
-        {
-            public string category_id { get; set; }
-            public string category_name { get; set; }
-            public string tobe_flag { get; set; }
-            public string flag_period { get; set; }
-            public string flag_data { get; set; }
-            public string today { get; set; }
-        }
 
-        //to turn all categories for intitutional file
-        public static List<OrgCategory> LoadOrgCategory()
+
+        //to turn all categories for institutional file
+        public List<InstitutionalOrgCategory> LoadOrgCategory()
         {
-            System.Data.SqlClient.SqlConnection conn = new System.Data.SqlClient.SqlConnection(ConfigurationManager.ConnectionStrings["EgrantsDB"].ConnectionString);
             System.Data.SqlClient.SqlCommand cmd = new System.Data.SqlClient.SqlCommand("SELECT doctype_id AS category_id, doctype_name AS category_name, tobe_flagged AS tobe_flag, Flag_period FROM dbo.Org_Categories ORDER BY category_name", conn);
             cmd.CommandType = CommandType.Text;
 
             conn.Open();
 
-            var OrgCategoryList = new List<OrgCategory>();
+            var OrgCategoryList = new List<InstitutionalOrgCategory>();
             SqlDataReader rdr = cmd.ExecuteReader();
             while (rdr.Read())
             {
-                OrgCategoryList.Add(new OrgCategory
+                OrgCategoryList.Add(new InstitutionalOrgCategory
                 {
                     category_id= rdr["category_id"]?.ToString(),
                     category_name = rdr["category_name"]?.ToString(),
@@ -182,10 +149,10 @@ namespace egrants_new.Egrants.Models
             return OrgCategoryList;
         }
 
-        //to disable an intitutional file
-        public static void DisableDoc(string act, string str, int index_id, int org_id, int doc_id, int category_id, string file_type, string start_date, string end_date, string ic, string userid)
+        //to disable an institutional file
+        public void DisableDoc(string act, string str, int index_id, int org_id, int doc_id, int category_id, string file_type, string start_date, string end_date, string ic, string userid)
         {
-            System.Data.SqlClient.SqlConnection conn = new System.Data.SqlClient.SqlConnection(ConfigurationManager.ConnectionStrings["EgrantsDB"].ConnectionString);
+
             System.Data.SqlClient.SqlCommand cmd = new System.Data.SqlClient.SqlCommand("sp_web_egrants_institutional_files", conn);
             cmd.CommandType = CommandType.StoredProcedure;
 
@@ -208,8 +175,8 @@ namespace egrants_new.Egrants.Models
             conn.Close();
         }
 
-        //to create new intitutional file and return document_id
-        public static string GetDocID(int org_id, int category_id, string file_type, string start_date, string end_date, string ic, string userid)
+        //to create new institutional file and return document_id
+        public string GetDocID(int org_id, int category_id, string file_type, string start_date, string end_date, string ic, string userid)
         {
             System.Data.SqlClient.SqlConnection conn = new System.Data.SqlClient.SqlConnection(ConfigurationManager.ConnectionStrings["EgrantsDB"].ConnectionString);
             System.Data.SqlClient.SqlCommand cmd = new System.Data.SqlClient.SqlCommand("sp_web_egrants_institutional_file_create", conn);
