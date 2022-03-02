bf4eeade0b56529d3522723677c60537cfbf4128
benny shell <xosippa@gmail.com>
2022-03-02 00:08:02 -0500

    Incremental Fixes SQL Updates and changes to support better practices

---
+++ sql-definitions/EIM/StoredProcedures/dbo.sp_web_egrants_inst_files_disable_doc.sql
@@ -0,0 +1,30 @@
+﻿SET ANSI_NULLS OFF
+SET QUOTED_IDENTIFIER OFF
+
+
+
+Create    PROCEDURE [dbo].[sp_web_egrants_inst_files_disable_doc]
+(
+@person_id	int,
+@doc_id		int
+)
+
+AS
+/************************************************************************************************************/
+/***									 									***/
+/***	Procedure Name:sp_web_egrants_inst_files_disable_doc     			***/
+/***	Description:files disable doc 										***/
+/***	Created:	03/01/2022	Madhu										***/
+/************************************************************************************************************/
+SET NOCOUNT ON
+
+-------------------
+-- disable_doc:
+
+update dbo.Org_Document set disabled_date=GETDATE(), disabled_by_person_id=@person_id where document_id=@doc_id	
+
+RETURN
+
+
+GO
+
