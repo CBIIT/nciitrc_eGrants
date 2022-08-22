df77658552e6a4c2aff34d813bf20348b0cfd8c3
Robin Briggs <robin.briggs@nih.gov>
2022-08-22 13:43:03 -0400

    Included debug statements to web console

---
+++ source-aspnet/egrants_new/Egrants/Models/DownloadModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace egrants_new.Egrants.Models
+{
+    public class DownloadModel
+    {
+        /// <summary>
+        /// Gets or sets the appl_id.
+        /// </summary>
+        public string ApplId { get; set; }
+
+        public string Handle { get; set; }
+
+        public List<DownloadData> DownloadDataList { get; set; }
+    }
+
+    public class DownloadData
+    {
+        public string Url { get; set; }
+
+        public string FileDownloaded { get; set; }
+
+        public string Error { get; set; }
+    }
+}
