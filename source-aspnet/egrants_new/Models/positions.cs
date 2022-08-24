af9d588030ce7a62616bc8a689c34b1e1067ce62
Robin Briggs <robin.briggs@nih.gov>
2022-08-24 13:35:01 -0400

    Split out models from EgrantsCommon and also some code corrections

---
+++ source-aspnet/egrants_new/Models/positions.cs
@@ -0,0 +1,58 @@
+﻿#region FileHeader
+
+// /****************************** Module Header ******************************\
+// Module Name:  EgrantsCommon.cs
+// Solution: egrants_new
+// Project:  egrants_new
+// Created: 2022-05-05
+// Contributors:
+//      - Briggs, Robin (NIH/NCI) [C] - briggsr2
+//      -
+// Copyright (c) National Institute of Health
+// 
+// <Description of the file>
+// 
+// This source is subject to the NIH Softwre License.
+// See https://ncihub.org/resources/899/download/Guidelines_for_Releasing_Research_Software_04062015.pdf
+// All other rights reserved.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
+// WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT ARE DISCLAIMED. IN NO EVENT SHALL THE NATIONAL
+// CANCER INSTITUTE (THE PROVIDER), THE NATIONAL INSTITUTES OF HEALTH, THE
+// U.S. GOVERNMENT OR THE INDIVIDUAL DEVELOPERS BE LIABLE FOR ANY DIRECT,
+// INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+// HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+// STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+// \***************************************************************************/
+
+#endregion
+
+#region
+
+
+#endregion
+
+namespace egrants_new.Models
+{
+    /// <summary>
+    ///     The positions.
+    /// </summary>
+    public class Position
+    {
+        /// <summary>
+        ///     Gets or sets the position_id.
+        /// </summary>
+        internal string PositionId { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the position_name.
+        /// </summary>
+        internal string PositionName { get; set; }
+    }
+}
