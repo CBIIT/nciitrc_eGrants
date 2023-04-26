88a1ea9d396b826415f7875010d1a2af263ca964
Robin Briggs <robin.briggs@nih.gov>
2023-04-26 10:43:43 -0400

    Ugraded to 4.8 and obtained all 4.8 packages.

--- source-aspnet/egrants_new/Global.asax.cs
+++ source-aspnet/egrants_new/Global.asax.cs
@@ -132,7 +132,7 @@ namespace egrants_new
 
             //TelemetryConfiguration.Active.DisableTelemetry = true;
 
-            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls13;
             this.Application["UsersOnline"] = 0;
 
             ViewEngines.Engines.Clear();
