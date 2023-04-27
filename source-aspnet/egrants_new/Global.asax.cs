57fe5b4b26a662f3adb7edade39ac5da4226206c
Robin Briggs <robin.briggs@nih.gov>
2023-04-27 15:42:55 -0400

    remobe bundle.config

--- source-aspnet/egrants_new/Global.asax.cs
+++ source-aspnet/egrants_new/Global.asax.cs
@@ -146,7 +146,7 @@ namespace egrants_new
 
             RouteConfig.RegisterRoutes(RouteTable.Routes);
 
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            //BundleConfig.RegisterBundles(BundleTable.Bundles);
             //  HangfireAspNet.Use(this.GetHangfireServers);
 
             // var wsCronExp = ConfigurationManager.AppSettings[@"IntegrationCheckCronExp"];
