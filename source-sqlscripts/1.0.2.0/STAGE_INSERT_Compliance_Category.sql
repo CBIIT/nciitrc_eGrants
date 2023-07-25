fda2ec2aa988e0f9184bd2c97fd84d39e7f17894
Robin Briggs <robin.briggs@nih.gov>
2023-07-25 15:00:12 -0400

    Added SQL stagement to Tag 1.0.1.2 for the update of Stage and Prod with "Compliance" category

---
+++ source-sqlscripts/1.0.2.0/STAGE_INSERT_Compliance_Category.sql
@@ -0,0 +1,32 @@
+USE [EIM]
+GO
+
+  INSERT INTO [dbo].[categories]
+           ([category_name]
+           ,[created_date]
+           ,[created_by_person_id]
+           ,[can_upload]
+           ,[input_type]
+           ,[input_constraint])
+     VALUES
+           ('Compliance',
+           GETDATE()
+           ,1899
+           ,'Yes'
+           ,'T'
+           ,0)
+
+ INSERT INTO [dbo].[categories_ic]
+           ([category_id]
+           ,[ic]
+           ,[added_date]
+           ,[added_by_person_id])
+     VALUES
+           (676,
+            'NCI'
+           ,GETDATE()
+           ,1899)
+
+GO
+
+
