4c5fb6c28a03b5390442ac626d69722acec47c88
micahhooverNIH <micah.hoover@nih.com>
2023-09-20 11:02:17 -0400

    Added script to adjust existing sproc (sp_egrants_maint_check_expired_people) to deactivate accounts after 60 days of inactivity (down from 120 days)

---
+++ source-sqlscripts/1.0.2.1/eGrants308_revoke_access_after_60days_nonactivity.sql
@@ -0,0 +1,22 @@
+USE [EIM]
+GO
+/****** Object:  StoredProcedure [dbo].[sp_egrants_maint_check_expired_people]    Script Date: 9/20/2023 10:34:06 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+ALTER   PROCEDURE [dbo].[sp_egrants_maint_check_expired_people]
+AS
+	PRINT '----------- Deactivating Users Who Have Not Recently Logged In ----------'
+	PRINT 'Setting these people to Active = 0'
+	
+	DECLARE @sql varchar(max)
+	SET @sql='select * from people WHERE last_login_date < DateAdd(day, -120, GETDATE()) AND Active=1'
+	EXEC(@sql)
+	PRINT(@sql)
+
+	UPDATE [dbo].[people]
+	SET active = 0
+	WHERE last_login_date is not null AND last_login_date < DateAdd(day, -60, GETDATE()) AND active=1;
