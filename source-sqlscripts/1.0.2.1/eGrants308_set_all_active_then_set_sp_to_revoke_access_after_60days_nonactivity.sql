85186d1fd49b3f35def82bba6ee758e65bf96644
micahhooverNIH <micah.hoover@nih.com>
2023-09-28 08:08:04 -0400

    Tested and renamed sproc change script

---
+++ source-sqlscripts/1.0.2.1/eGrants308_set_all_active_then_set_sp_to_revoke_access_after_60days_nonactivity.sql
@@ -0,0 +1,32 @@
+USE [EIM]
+GO
+/****** Object:  StoredProcedure [dbo].[sp_egrants_maint_check_expired_people]    Script Date: 9/20/2023 10:34:06 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+	PRINT '----------- reset everyone to active ----------' 
+	select TOP 1000 last_login_date from people
+	UPDATE [dbo].[people]
+	SET active = 1, last_login_date = GETDATE()
+	
+GO
+
+PRINT '----------- Adjusting [dbo].[sp_egrants_maint_check_expired_people] to 60 day expiration ----------'
+
+GO
+
+ALTER   PROCEDURE [dbo].[sp_egrants_maint_check_expired_people]
+AS
+	PRINT '----------- Deactivating Users Who Have Not Recently Logged In ----------'
+	PRINT 'Setting these people to Active = 0'
+	
+	DECLARE @sql varchar(max)
+	SET @sql='select * from people WHERE last_login_date < DateAdd(day, -60, GETDATE()) AND Active=1'
+	EXEC(@sql)
+	PRINT(@sql)
+
+	UPDATE [dbo].[people]
+	SET active = 0
+	WHERE last_login_date is not null AND last_login_date < DateAdd(day, -60, GETDATE()) AND active=1;
