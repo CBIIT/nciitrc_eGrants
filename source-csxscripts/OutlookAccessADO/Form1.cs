bb7eb86bcccca5c8bf77d0a5343845eb0e5e3ea5
micahhooverNIH <micah.hoover@nih.com>
2024-05-16 10:46:02 -0400

    CSX script is reading config and creating logs and C# is reading outlook emails

---
+++ source-csxscripts/OutlookAccessADO/Form1.cs
@@ -0,0 +1,68 @@
+using System.Data;
+using System.Data.OleDb;
+
+using Outlook = Microsoft.Office.Interop.Outlook;
+
+namespace OutlookAccessADO
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String sqlText = "SELECT Subject, Contents FROM Inbox";
+
+            // Build the connection string.
+            String connectionString = "Provider=Microsoft.Jet.OLEDB.4.0;" +
+                "Outlook 9.0;" +
+                //"MAPILEVEL=" + mailboxNameTextBox.Text + "|;" +
+                // "PROFILE=" + profileTextBox.Text + ";" +
+                "TABLETYPE=0;" +
+                "DATABASE=" + System.IO.Path.GetTempPath();
+
+            // Create the DataAdapter.
+            OleDbDataAdapter da = new OleDbDataAdapter(sqlText, connectionString);
+
+            // Create and fill the table.
+            DataTable dt = new DataTable("Inbox");
+            try
+            {
+                da.Fill(dt);
+                // dataGrid.DataSource = dt.DefaultView;
+                outputTextBox.Text = dt.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("ERROR: " + ex.Message);
+                return;
+            }
+
+        }
+
+        private void interOpButton_Click(object sender, EventArgs e)
+        {
+            // Office 365 should reference Office 16 ... prolly different from the version listed on the DLL ...
+
+            Console.WriteLine("Here we go ...");
+            Outlook.Application oApp = new Outlook.Application();
+            Console.WriteLine("Created the object ...");
+            Outlook.NameSpace oNS = oApp.GetNamespace("MAPI");
+            oNS.Logon("", "", false, true);
+            Outlook.MAPIFolder oInbox = oNS.GetDefaultFolder(Outlook.OlDefaultFolders.olFolderInbox);
+            Outlook.Items oItems = oInbox.Items;
+            Console.WriteLine(oItems.Count);
+            Outlook.MailItem oMsg = (Outlook.MailItem)oItems.GetFirst();
+            Console.WriteLine(oMsg.Subject);
+            oNS.Logoff();
+        }
+    }
+}
