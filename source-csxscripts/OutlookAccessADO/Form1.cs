0056303d1d215536f03368b1e061d61d39cd0fb3
micahhooverNIH <micah.hoover@nih.com>
2024-05-16 11:18:02 -0400

    OK, removing the hard coded GAC DLL seemed to work

--- source-csxscripts/OutlookAccessADO/Form1.cs
+++ source-csxscripts/OutlookAccessADO/Form1.cs
@@ -1,6 +1,6 @@
 using System.Data;
 using System.Data.OleDb;
-
+using System.Text;
 using Outlook = Microsoft.Office.Interop.Outlook;
 
 namespace OutlookAccessADO
@@ -52,17 +52,25 @@ namespace OutlookAccessADO
         {
             // Office 365 should reference Office 16 ... prolly different from the version listed on the DLL ...
 
+            var sb = new StringBuilder();
+
             Console.WriteLine("Here we go ...");
+            sb.Append("Here we go ...\r\n");
             Outlook.Application oApp = new Outlook.Application();
             Console.WriteLine("Created the object ...");
+            sb.Append("Created the object ...\r\n");
             Outlook.NameSpace oNS = oApp.GetNamespace("MAPI");
             oNS.Logon("", "", false, true);
             Outlook.MAPIFolder oInbox = oNS.GetDefaultFolder(Outlook.OlDefaultFolders.olFolderInbox);
             Outlook.Items oItems = oInbox.Items;
             Console.WriteLine(oItems.Count);
+            sb.Append($"Found {oItems.Count} many emails\r\n");
             Outlook.MailItem oMsg = (Outlook.MailItem)oItems.GetFirst();
             Console.WriteLine(oMsg.Subject);
+            sb.Append($"Here's the first subject : {oMsg.Subject}\r\n");
             oNS.Logoff();
+
+            outputTextBox.Text = sb.ToString();
         }
     }
 }
