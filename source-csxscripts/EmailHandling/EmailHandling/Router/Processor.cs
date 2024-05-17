297faf6eca2f19b5bcf4a88545c0419bd34af39a
micahhooverNIH <micah.hoover@nih.com>
2024-05-17 13:22:29 -0400

    C# is now processing through the outlook public folders

---
+++ source-csxscripts/EmailHandling/EmailHandling/Router/Processor.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Outlook = Microsoft.Office.Interop.Outlook;
+
+namespace Router
+{
+    internal class Processor
+    {
+        public static int Process(string dirPath, SqlConnection con, string verbose, string debug)
+        {
+            int itemsProcessedCount = 0;
+
+            //Utilities.ShowDiagnosticIfVerbose($"_conStr: '{_conStr}'", _verbose);
+
+            Utilities.ShowDiagnosticIfVerbose("Here we go ...", verbose);
+            Outlook.Application oApp = new Outlook.Application();
+            Utilities.ShowDiagnosticIfVerbose("Created the outlook object.", verbose);
+            Outlook.NameSpace oNS = oApp.GetNamespace("MAPI");
+            oNS.Logon("", "", false, true);
+            Utilities.ShowDiagnosticIfVerbose($"Logged on to Outlook.", verbose);
+
+            char sepchar = '\\';
+
+            Utilities.ShowDiagnosticIfVerbose($"dirpath: {dirPath}", verbose);
+            //Parse inputstr and Navigate to the folder
+            if (!string.IsNullOrWhiteSpace(dirPath))
+            {
+                var dirs = dirPath.Split(sepchar);
+                var i = 0;
+                Utilities.ShowDiagnosticIfVerbose($"Setting objNS.Folders to {dirs[0]}", verbose);
+                Outlook.MAPIFolder startingFolder = null;
+                foreach (Outlook.MAPIFolder folder in oNS.Folders)
+                {
+                    Utilities.ShowDiagnosticIfVerbose($"Checking folder named ... {folder.Name} to see if it looks like '{dirs[i]}'", verbose);
+                    if (folder.Name.Equals(dirs[i]))
+                    {
+                        Utilities.ShowDiagnosticIfVerbose($"Checking folder named ... {folder.Name}", verbose);
+                        startingFolder = folder;
+                    }
+                }
+                Utilities.ShowDiagnosticIfVerbose("Set", verbose);
+                Outlook.MAPIFolder currentFolder = startingFolder;
+
+                // loop through all the folders
+                i++;
+                while(i < dirs.Length - 1)
+                {
+                    bool foundThisDirectory = false;
+                    Utilities.ShowDiagnosticIfVerbose($"dirs {i} : {dirs[i]}", verbose);
+
+                    foreach (Outlook.MAPIFolder folder in currentFolder.Folders)
+                    {
+                        Utilities.ShowDiagnosticIfVerbose($"Checking folder named ... {folder.Name} to see if it looks like '{dirs[i]}'", verbose);
+                        if (folder.Name.Equals(dirs[i]))
+                        {
+                            foundThisDirectory = true;
+                            Utilities.ShowDiagnosticIfVerbose($"Checking folder named ... {folder.Name}", verbose);
+                            currentFolder = folder;
+                            break;
+                        }
+                    }
+                    if (foundThisDirectory)
+                    {
+                        i++;
+                    }
+                }
+            }
+
+
+		//Do While i < UBound(xArray)
+
+  //          call ShowDiagnosticIfVerbose("xArray i " & xArray(i), Verbose)
+		//	Set CFolder = CFolder.Folders(xArray(i))
+
+  //          i = i + 1
+
+  //      Loop
+  //  End If  'If dirpath <> "" Then
+
+            //oNS.Folders()
+            //Outlook.MAPIFolder oInbox = oNS.GetFolderFromID()
+            //Outlook.GetDefaultFolder(Outlook.OlDefaultFolders.olFolderInbox);
+            //Outlook.Items oItems = oInbox.Items;
+            //Console.WriteLine(oItems.Count);
+            //sb.Append($"Found {oItems.Count} many emails\r\n");
+            //Outlook.MailItem oMsg = (Outlook.MailItem)oItems.GetFirst();
+            //Console.WriteLine(oMsg.Subject);
+            //sb.Append($"Here's the first subject : {oMsg.Subject}\r\n");
+            //oNS.Logoff();
+            //Console.ReadLine();
+
+            return itemsProcessedCount;
+        }
+    }
+}
--- source-csxscripts/EmailHandling/EmailHandling/Router/Program.cs
+++ source-csxscripts/EmailHandling/EmailHandling/Router/Program.cs
@@ -1,26 +1,84 @@
-﻿using System.Data;
-using System.Data.OleDb;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
+
+using System.Data;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+using Router;
 using Outlook = Microsoft.Office.Interop.Outlook;
 
 
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+namespace Router
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
 
-var sb = new StringBuilder();
+            var _startTimeStamp = DateTime.Now;
 
-Console.WriteLine("Here we go ...");
-sb.Append("Here we go ...\r\n");
-Outlook.Application oApp = new Outlook.Application();
-Console.WriteLine("Created the object ...");
-sb.Append("Created the object ...\r\n");
-Outlook.NameSpace oNS = oApp.GetNamespace("MAPI");
-oNS.Logon("", "", false, true);
-Outlook.MAPIFolder oInbox = oNS.GetDefaultFolder(Outlook.OlDefaultFolders.olFolderInbox);
-Outlook.Items oItems = oInbox.Items;
-Console.WriteLine(oItems.Count);
-sb.Append($"Found {oItems.Count} many emails\r\n");
-Outlook.MailItem oMsg = (Outlook.MailItem)oItems.GetFirst();
-Console.WriteLine(oMsg.Subject);
-sb.Append($"Here's the first subject : {oMsg.Subject}\r\n");
-oNS.Logoff();
+            var _verbose = Utilities.GetConfigVal("Verbose");
+            Utilities.ShowDiagnosticIfVerbose($"_verbose: '{_verbose}'", _verbose);
+            var _debug = Utilities.GetConfigVal("dBug");
+            Utilities.ShowDiagnosticIfVerbose($"_debug: '{_debug}'", _verbose);
+            var _logDir = Utilities.GetConfigVal("logDir");
+            Utilities.LogDir = _logDir;
+            Utilities.ShowDiagnosticIfVerbose($"_logDir: '{_logDir}'", _verbose);
+            var _conStr = Utilities.GetConfigVal("conStr");
+            Utilities.ShowDiagnosticIfVerbose($"_conStr: '{_conStr}'", _verbose);
+            var _dirPath = Utilities.GetConfigVal("dirpathRouter");
+            Utilities.ShowDiagnosticIfVerbose($"_dirPath: '{_dirPath}'", _verbose);
+
+            Utilities.ShowDiagnosticIfVerbose("Running the router", _verbose);
+
+            int _forAppending = 8;
+            var _taskStartMssg = "...........Task Started!...........";
+            Utilities.WriteLog(_forAppending, _taskStartMssg, null, _startTimeStamp);
+
+            var _dBugEmail = "leul.ayana@nih.gov";
+            var _eGrantsDevEmail = "eGrantsDev@mail.nih.gov";
+            var _eGrantsTestEmail = "eGrantsTest1@mail.nih.gov";
+            var _eGrantsStageEmail = "eGrantsStage@mail.nih.gov";
+            var _eFileEmail = "efile@mail.nih.gov";
+            var _nciGrantsPostAwardEmail = "NCIGrantsPostAward@nih.gov";
+
+            // System.ArgumentException: 'Keyword not supported: 'provider'.'
+            SqlConnection _con = new SqlConnection(_conStr);
+
+
+            var _itemsProcessed = Processor.Process(_dirPath, _con, _verbose, _debug);
+
+            var _taskEndMssg = $"******* Task Completed! ******* {_itemsProcessed} Mail Items Have Been Processed";
+            var _endTimeStamp = DateTime.Now;
+            Utilities.WriteLog(_forAppending, _taskEndMssg, null, _endTimeStamp);
+
+
+
+            // See https://aka.ms/new-console-template for more information
+            Console.WriteLine("Hello, World!");
+
+            var sb = new StringBuilder();
+
+            Console.WriteLine("Here we go ...");
+            sb.Append("Here we go ...\r\n");
+            Outlook.Application oApp = new Outlook.Application();
+            Console.WriteLine("Created the object ...");
+            sb.Append("Created the object ...\r\n");
+            Outlook.NameSpace oNS = oApp.GetNamespace("MAPI");
+            oNS.Logon("", "", false, true);
+            Outlook.MAPIFolder oInbox = oNS.GetDefaultFolder(Outlook.OlDefaultFolders.olFolderInbox);
+            Outlook.Items oItems = oInbox.Items;
+            Console.WriteLine(oItems.Count);
+            sb.Append($"Found {oItems.Count} many emails\r\n");
+            Outlook.MailItem oMsg = (Outlook.MailItem)oItems.GetFirst();
+            Console.WriteLine(oMsg.Subject);
+            sb.Append($"Here's the first subject : {oMsg.Subject}\r\n");
+            oNS.Logoff();
+            Console.ReadLine();
+
+        }
+    }
+}
