33d718d85cd653e5da4374ee1cb49dde0c0c3b9e
micahhooverNIH <micah.hoover@nih.com>
2024-05-31 12:16:25 -0400

    All unit tests passing for C# routing

---
+++ source-csxscripts/EmailHandling/EmailHandling/EmailTests/ClinicalTrialDotGov.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Outlook = Microsoft.Office.Interop.Outlook;
+
+namespace EmailTests
+{
+    [TestClass]
+    public class ClinicalTrialDotGovRequiredTests
+    {
+        private string _eGrantsDevEmail = "eGrantsDev@mail.nih.gov";
+        private string _josniEmail = "jonesni@mail.nih.gov";
+
+        // MLH : Note I haven't seen any emails with a subject that capture on this
+
+        [TestMethod]
+        public void ClinicalTrialDotGovRequiredSendToDevEmail()
+        {
+            // Arrange
+            Outlook.Application oApp = new Outlook.Application();
+            var testEmail = (Outlook.MailItem)oApp.CreateItem(Outlook.OlItemType.olMailItem);
+            var Subject = "ClinicalTrials.gov Results Reporting for Grant 5R01CA228019-05 - This subject is totally fabricated, find one from production";
+            testEmail.Subject = Subject;
+            var Body = " \r\n";
+            testEmail.Body = Body;
+            var testProcessor = new TestProcessor();
+
+            // Act
+            var sentResults = testProcessor.TestSingleEmail(testEmail);
+
+            // Assert
+            Assert.IsTrue(sentResults["recipients"].Contains(_eGrantsDevEmail));
+        }
+
+        [TestMethod]
+        public void ClinicalTrialDotGovRequiredLastFourDigits()
+        {
+            // Arrange
+            Outlook.Application oApp = new Outlook.Application();
+            var testEmail = (Outlook.MailItem)oApp.CreateItem(Outlook.OlItemType.olMailItem);
+            var Subject = "ClinicalTrials.gov Results Reporting for Grant 5R01CA228019-05 - This subject is totally fabricated, find one from production";
+            testEmail.Subject = Subject;
+            var Body = " \r\n";
+            testEmail.Body = Body;
+            var testProcessor = new TestProcessor();
+
+            // Act
+            var sentResults = testProcessor.TestSingleEmail(testEmail);
+
+            // Assert
+            var subj = sentResults["subject"];
+            Assert.IsTrue(subj.Contains("Results Reporting Reminder NCTtion")); // the last four digits here ("tion") are from the subject above, again find an actual email and adjust this test
+        }
+
+        [TestMethod]
+        public void ClinicalTrialDotGovRequiredApplId()
+        {
+            // Arrange
+            Outlook.Application oApp = new Outlook.Application();
+            var testEmail = (Outlook.MailItem)oApp.CreateItem(Outlook.OlItemType.olMailItem);
+            var Subject = "ClinicalTrials.gov Results Reporting for Grant 5R01CA228019-05 - This subject is totally fabricated, find one from production";
+            testEmail.Subject = Subject;
+            var Body = " \r\n";
+            testEmail.Body = Body;
+            var testProcessor = new TestProcessor();
+
+            // Act
+            var sentResults = testProcessor.TestSingleEmail(testEmail);
+
+            // Assert
+            var subj = sentResults["subject"];
+            Assert.IsTrue(subj.Contains("applid=10532149"));
+        }
+
+        [TestMethod]
+        public void ClinicalTrialDotGovRequiredCheckedSubject()
+        {
+            // Arrange
+            Outlook.Application oApp = new Outlook.Application();
+            var testEmail = (Outlook.MailItem)oApp.CreateItem(Outlook.OlItemType.olMailItem);
+            var Subject = "ClinicalTrials.gov Results Reporting for Grant 5R01CA228019-05 - This subject is totally fabricated, find one from production";
+            testEmail.Subject = Subject;
+            var Body = " \r\n";
+            testEmail.Body = Body;
+            var testProcessor = new TestProcessor();
+
+            // Act
+            var sentResults = testProcessor.TestSingleEmail(testEmail);
+
+            // Assert
+            var subj = sentResults["subject"];
+            Assert.IsTrue(subj.Contains("category=CT.gov, sub=Results Reporting Reminder NCT"));
+        }
+
+
+        [TestMethod]
+        public void ClinicalTrialDotGovRequiredSameSubjectNegative()
+        {
+            // Arrange
+            Outlook.Application oApp = new Outlook.Application();
+            var testEmail = (Outlook.MailItem)oApp.CreateItem(Outlook.OlItemType.olMailItem);
+            var Subject = "ClinicalTrials.com Results Reporting for Grant 5R01CA228019-05 - This subject is totally fabricated, find one from production";       //      <----- an off subject (com instead of gov)
+            testEmail.Subject = Subject;
+            var Body = " \r\n";
+            testEmail.Body = Body;
+            var testProcessor = new TestProcessor();
+
+            // Act
+            var sentResults = testProcessor.TestSingleEmail(testEmail);
+
+            // Assert
+            Assert.IsFalse(sentResults.ContainsKey("subject"));
+        }
+
+    }
+}
