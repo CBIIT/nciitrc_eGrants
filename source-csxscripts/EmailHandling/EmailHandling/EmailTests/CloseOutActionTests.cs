a8d75856d8b74e9638c764292776cd2a438ef03a
micahhooverNIH <micah.hoover@nih.com>
2024-05-31 12:16:25 -0400

    All unit tests passing for C# routing

---
+++ source-csxscripts/EmailHandling/EmailHandling/EmailTests/CloseOutActionTests.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Outlook = Microsoft.Office.Interop.Outlook;
+
+namespace EmailTests
+{
+    [TestClass]
+    public class CloseoutActionRequiredTests
+    {
+        private string _eGrantsDevEmail = "eGrantsDev@mail.nih.gov";
+        private string _josniEmail = "jonesni@mail.nih.gov";
+
+        [TestMethod]
+        public void CloseoutActionRequiredSendToDevEmail()
+        {
+            // Arrange
+            Outlook.Application oApp = new Outlook.Application();
+            var testEmail = (Outlook.MailItem)oApp.CreateItem(Outlook.OlItemType.olMailItem);
+            var Subject = "Closeout Action Required: 1R44CA256984-01A1 - Pasche, Valerie Past Due Closeout Documents";
+            testEmail.Subject = Subject;
+            var Body = " \r\n";
+            testEmail.Body = Body;
+            var testProcessor = new TestProcessor();
+
+            // Act
+            var sentResults = testProcessor.TestSingleEmail(testEmail);
+
+            // Assert
+            Assert.IsTrue(sentResults["recipients"].Contains(_eGrantsDevEmail));
+        }
+
+        [TestMethod]
+        public void CloseoutActionRequiredApplId()
+        {
+            // Arrange
+            Outlook.Application oApp = new Outlook.Application();
+            var testEmail = (Outlook.MailItem)oApp.CreateItem(Outlook.OlItemType.olMailItem);
+            var Subject = "Closeout Action Required: 1R44CA256984-01A1 - Pasche, Valerie Past Due Closeout Documents";
+            testEmail.Subject = Subject;
+            var Body = " \r\n";
+            testEmail.Body = Body;
+            var testProcessor = new TestProcessor();
+
+            // Act
+            var sentResults = testProcessor.TestSingleEmail(testEmail);
+
+            // Assert
+            var subj = sentResults["subject"];
+            Assert.IsTrue(subj.Contains("applid=10254967"));
+        }
+
+        [TestMethod]
+        public void CloseoutActionRequiredCheckedSubject()
+        {
+            // Arrange
+            Outlook.Application oApp = new Outlook.Application();
+            var testEmail = (Outlook.MailItem)oApp.CreateItem(Outlook.OlItemType.olMailItem);
+            var Subject = "Closeout Action Required: 1R44CA256984-01A1 - Pasche, Valerie Past Due Closeout Documents";
+            testEmail.Subject = Subject;
+            var Body = " \r\n";
+            testEmail.Body = Body;
+            var testProcessor = new TestProcessor();
+
+            // Act
+            var sentResults = testProcessor.TestSingleEmail(testEmail);
+
+            // Assert
+            var subj = sentResults["subject"];
+            Assert.IsTrue(subj.Contains("category=closeout, sub=Past Due Documents Reminder"));
+        }
+
+
+        [TestMethod]
+        public void CloseoutActionRequiredSameSubjectNegative()
+        {
+            // Arrange
+            Outlook.Application oApp = new Outlook.Application();
+            var testEmail = (Outlook.MailItem)oApp.CreateItem(Outlook.OlItemType.olMailItem);
+            var Subject = "Closeout Action Passiveness: 1R44CA256984-01A1 - Pasche, Valerie Past Due Closeout Documents";       //      <----- an off subject (Passiveness)
+            testEmail.Subject = Subject;
+            var Body = " \r\n";
+            testEmail.Body = Body;
+            var testProcessor = new TestProcessor();
+
+            // Act
+            var sentResults = testProcessor.TestSingleEmail(testEmail);
+
+            // Assert
+            Assert.IsFalse(sentResults.ContainsKey("subject"));
+        }
+
+    }
+}
