73843c837db1c2a2743fccc0bc2d3f6814bf3238
micahhooverNIH <micah.hoover@nih.com>
2024-08-12 14:57:58 -0400

    Initial commit ... works locally with no vcron

---
+++ EmailHandling/OGARequestAccountDisable/DisabledListItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OGARequestAccountDisable
+{
+    public class DisabledListItem
+    {
+        public int PersonIdFromDB { get; set; }
+        public string EmailFromDB { get; set; }
+
+        public string PersonNameFromDB { get; set; }
+        public string FirstNameFromDB { get; set; }
+        public string LastNameFromDB { get; set; }
+
+        public string FinalNameForOGA { get; set; }
+
+        public bool FailedToRenderName { get; set; }
+    }
+}
