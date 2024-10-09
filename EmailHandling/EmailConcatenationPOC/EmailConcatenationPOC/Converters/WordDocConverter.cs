a623a5278c9d727c1ead109544bff111720b2d58
micahhooverNIH <micah.hoover@nih.com>
2024-10-09 15:07:27 -0400

    .doc files are now converting to PDF

---
+++ EmailHandling/EmailConcatenationPOC/EmailConcatenationPOC/Converters/WordDocConverter.cs
@@ -0,0 +1,40 @@
+﻿using EmailConcatenationPOC.Interfaces;
+using IronPdf;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Spire.Doc;
+
+namespace EmailConcatenationPOC.Converters
+{
+    public class WordDocConverter : IWordDocConverter, IConvertToPdf
+    {
+        public bool SupportsThisFileType(string fileName)
+        {
+            if (!string.IsNullOrWhiteSpace(fileName) &&
+                fileName.ToLower().EndsWith(".doc") )
+                return true;
+            return false;
+        }
+
+        public PdfDocument ToPdfDocument(ContentForPdf content)
+        {
+            Console.WriteLine("Handling Word .doc file type case ...");
+
+            using (var memoryStream = new MemoryStream(content.Attachment.Data))
+            {
+                Document document = new Document(memoryStream);
+                using (var pdfStream = new MemoryStream())
+                {
+                    document.SaveToStream(pdfStream, FileFormat.PDF);
+                    pdfStream.Position = 0;
+                    var newPdfFile = new PdfDocument(pdfStream);
+                    return newPdfFile;
+                }
+            }
+        }
+    }
+}
