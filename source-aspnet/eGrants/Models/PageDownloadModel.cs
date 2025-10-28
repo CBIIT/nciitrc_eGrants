3c670e97a9ad25084e9bb6576984defb54066ef3
Daryl-Dehuff <daryl.dehuff@nih.gov>
2025-10-28 13:02:49 -0400

    Modified header comments for certain files.

--- source-aspnet/eGrants/Models/PageDownloadModel.cs
+++ source-aspnet/eGrants/Models/PageDownloadModel.cs
@@ -2,12 +2,11 @@
 
 // /****************************** Module Header ******************************\
 // Module Name:  PageDownloadModel.cs
-// Solution: egrants_new
-// Project:  egrants_new
-// Created: 2023-04-13
+// Solution: eGrants
+// Project:  eGrants
+// Created: 2025-08-01
 // Contributors:
-//      - Briggs, Robin (NIH/NCI) [C] - briggsr2
-//      -
+//      - Dehuff, Daryl (NIH/NCI) [C] - dehuffdc
 // Copyright (c) National Institute of Health
 // 
 // <Description of the file>
