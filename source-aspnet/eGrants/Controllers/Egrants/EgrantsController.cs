c3810ba6dc86e1a91e19e3d51b45f7309dc6e585
Daryl-Dehuff <daryl.dehuff@nih.gov>
2025-11-05 12:47:55 -0500

    Modifications required for eGrants-819 to get the page numbers to show up and the links for those page numbers to work.

--- source-aspnet/eGrants/Controllers/Egrants/EgrantsController.cs
+++ source-aspnet/eGrants/Controllers/Egrants/EgrantsController.cs
@@ -839,13 +839,13 @@ namespace eGrants.Controllers.Egrants
         /// <returns>
         /// The <see cref="ActionResult"/>.
         /// </returns>
-        public async Task<IActionResult> by_filters(int fiscalYear = 0, string mechanism = null, string adminCode = null, int serialNum = 0)
+        public async Task<IActionResult> by_filters(int fiscalYear = 0, string mechanism = null, string adminCode = null, int serialNum = 0, int pageNum = 1, int tabNum = 1, string packages = "")
         {
             eGrantsSearchViewModel eGrantsSearchViewModelList = new eGrantsSearchViewModel();
 
             var sessionInfo = _sessionInfoService.GetSessionInfo(HttpContext.Session);
 
-            eGrantsSearchViewModelList = await _eGrantsService.GetEgrantsByFilterAsync(fiscalYear, mechanism, serialNum, adminCode, 0, 0, 0, sessionInfo);
+            eGrantsSearchViewModelList = await _eGrantsService.GetEgrantsByFilterAsync(fiscalYear, mechanism, serialNum, adminCode, 0, 0, pageNum, sessionInfo, tabNum, packages);
             eGrantsSearchViewModelList.ICList = await _commonService.LoadAdminCodes();
 
             return View("~/Views/Index.cshtml", eGrantsSearchViewModelList);
