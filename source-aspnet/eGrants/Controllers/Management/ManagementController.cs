7fc2c30dd7defd491f41632b4ec63638606e6320
Daryl-Dehuff <daryl.dehuff@nih.gov>
2025-11-24 15:27:05 -0500

    Made modifications to get Management page to come up and get the QC Assignment Report working.

---
+++ source-aspnet/eGrants/Controllers/Management/ManagementController.cs
@@ -0,0 +1,228 @@
+﻿#region FileHeader
+
+// /****************************** Module Header ******************************\
+// Module Name:  ManagementController.cs
+// Solution: egrants_new
+// Project:  egrants_new
+// Created: 2025-11-24
+// Contributors:
+//      - Dehuff, Daryl (NIH/NCI) [C] - dehuffdc
+//      -
+// Copyright (c) National Institute of Health
+// 
+// <Description of the file>
+// 
+// This source is subject to the NIH Softwre License.
+// See https://ncihub.org/resources/899/download/Guidelines_for_Releasing_Research_Software_04062015.pdf
+// All other rights reserved.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
+// WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT ARE DISCLAIMED. IN NO EVENT SHALL THE NATIONAL
+// CANCER INSTITUTE (THE PROVIDER), THE NATIONAL INSTITUTES OF HEALTH, THE
+// U.S. GOVERNMENT OR THE INDIVIDUAL DEVELOPERS BE LIABLE FOR ANY DIRECT,
+// INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+// HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+// STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+// \***************************************************************************/
+
+#endregion
+
+#region
+
+using System;
+
+using eGrants.Models;
+using eGrants.Services;
+using eGrants.Services.Interfaces;
+
+using Microsoft.AspNetCore.Mvc;
+
+#endregion
+
+namespace eGrants.Controllers.Management
+{
+    /// <summary>
+    /// The management controller.
+    /// </summary>
+    public class ManagementController : Controller
+    {
+
+        // Injected dependencies: database context and product service
+
+        private readonly IManagementService _managementService;
+        private readonly ISessionInfoService _sessionInfoService = new SessionInfoService();
+
+        public ManagementController(IManagementService managementService, ISessionInfoService sessionInfoService)
+        {
+            _managementService = managementService;
+            _sessionInfoService = sessionInfoService;
+        }
+
+        private SessionInfo sessionInfo => _sessionInfoService.GetSessionInfo(HttpContext.Session);
+        /// <summary>
+        /// The index.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="ActionResult"/>.
+        /// </returns>
+        public async Task<ActionResult> Index()
+        {
+            // load egrants qc reasons
+            this.ViewBag.QCReasons = await _managementService.LoadQCReasons(Convert.ToString(sessionInfo.Ic));
+
+            // load egrants specialist list
+            this.ViewBag.Specialists = await _managementService.LoadSpecialists(Convert.ToString(sessionInfo.Ic));
+
+            // load qc persons list
+            this.ViewBag.QCPersons = await _managementService.LoadQCPersons(Convert.ToString(sessionInfo.Ic));
+
+            // load qc report
+            this.ViewBag.QCReport = await _managementService.LoadQCReport(Convert.ToString(sessionInfo.Ic));
+
+            //return this.View("~Views/Management/Index.cshtml");
+            return View("~/Views/Management/Index.cshtml");
+        }
+
+        /// <summary>
+        /// The to_ assign.
+        /// </summary>
+        /// <param name="qc_reason">
+        /// The qc_reason.
+        /// </param>
+        /// <param name="qc_person_id">
+        /// The qc_person_id.
+        /// </param>
+        /// <returns>
+        /// The <see cref="ActionResult"/>.
+        /// </returns>
+        public async Task<ActionResult> To_Assign(string qc_reason, string qc_person_id)
+        {
+            var act = "to_assign";
+            var qcperson_id = Convert.ToInt32(qc_person_id);
+            var person_id = 0;
+            var percent = 0;
+
+            _managementService.run_db(
+                act,
+                qcperson_id,
+                qc_reason,
+                percent,
+                person_id,
+                Convert.ToString(sessionInfo.Ic),
+                Convert.ToString(sessionInfo.UserId));
+
+            // load egrants qc reasons
+            this.ViewBag.QCReasons = await _managementService.LoadQCReasons(Convert.ToString(sessionInfo.Ic));
+
+            // load egrants specialist list
+            this.ViewBag.Specialists = await _managementService.LoadSpecialists(Convert.ToString(sessionInfo.Ic));
+
+            // load qc persons list
+            this.ViewBag.QCPersons = await _managementService.LoadQCPersons(Convert.ToString(sessionInfo.Ic));
+
+            // load qc report
+            this.ViewBag.QCReport = await _managementService.LoadQCReport(Convert.ToString(sessionInfo.Ic));
+
+            // return View("~/Management/Views/Index.cshtml");
+            return this.RedirectToAction("Index");
+        }
+
+        /// <summary>
+        /// The to_ remove.
+        /// </summary>
+        /// <param name="qc_reason">
+        /// The qc_reason.
+        /// </param>
+        /// <param name="qc_person_id">
+        /// The qc_person_id.
+        /// </param>
+        /// <returns>
+        /// The <see cref="ActionResult"/>.
+        /// </returns>
+        public async Task<ActionResult> To_Remove(string qc_reason, string qc_person_id)
+        {
+            var act = "to_remove";
+            var qcperson_id = Convert.ToInt32(qc_person_id);
+            var person_id = 0;
+            var percent = 0;
+
+            _managementService.run_db(
+                act,
+                qcperson_id,
+                qc_reason,
+                percent,
+                person_id,
+                Convert.ToString(sessionInfo.Ic),
+                Convert.ToString(sessionInfo.UserId));
+
+            // load egrants qc reasons
+            this.ViewBag.QCReasons = await _managementService.LoadQCReasons(Convert.ToString(sessionInfo.Ic));
+
+            // load egrants specialist list
+            this.ViewBag.Specialists = await _managementService.LoadSpecialists(Convert.ToString(sessionInfo.Ic));
+
+            // load qc persons list
+            this.ViewBag.QCPersons = await _managementService.LoadQCPersons(Convert.ToString(sessionInfo.Ic));
+
+            // load qc report
+            this.ViewBag.QCReport = await _managementService.LoadQCReport(Convert.ToString(sessionInfo.Ic));
+
+            // return View("~/Management/Views/Index.cshtml");
+            return this.RedirectToAction("Index");
+        }
+
+        /// <summary>
+        /// The to_ route.
+        /// </summary>
+        /// <param name="person_id">
+        /// The person_id.
+        /// </param>
+        /// <param name="percent">
+        /// The percent.
+        /// </param>
+        /// <param name="qc_person_id">
+        /// The qc_person_id.
+        /// </param>
+        /// <returns>
+        /// The <see cref="ActionResult"/>.
+        /// </returns>
+        public async Task<ActionResult> To_Route(string person_id, string percent, string qc_person_id)
+        {
+            var act = "to_route";
+            var qcperson_id = Convert.ToInt32(qc_person_id);
+            var personid = Convert.ToInt32(person_id);
+            var percents = Convert.ToInt32(percent);
+            var qc_reason = string.Empty;
+
+            _managementService.run_db(
+                act,
+                qcperson_id,
+                qc_reason,
+                percents,
+                personid,
+                Convert.ToString(sessionInfo.Ic),
+                Convert.ToString(sessionInfo.UserId));
+
+            // load egrants qc reasons
+            this.ViewBag.QCReasons = await _managementService.LoadQCReasons(Convert.ToString(sessionInfo.Ic));
+
+            // load egrants specialist list
+            this.ViewBag.Specialists = await _managementService.LoadSpecialists(Convert.ToString(sessionInfo.Ic));
+
+            // load qc persons list
+            this.ViewBag.QCPersons = await _managementService.LoadQCPersons(Convert.ToString(sessionInfo.Ic));
+
+            // load qc report
+            this.ViewBag.QCReport = await _managementService.LoadQCReport(Convert.ToString(sessionInfo.Ic));
+
+            // return View("~/Management/Views/Index.cshtml");
+            return this.RedirectToAction("Index");
+        }
+    }
+}
---
+++ source-aspnet/eGrants/Services/ManagementService.cs
@@ -0,0 +1,206 @@
+﻿using System.Data;
+using System.Runtime.CompilerServices;
+
+using eGrants.DAL;
+using eGrants.Models;
+using eGrants.Services.Interfaces;
+
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+
+using Serilog;
+
+namespace eGrants.Services
+{
+    public class ManagementService : IManagementService
+    {
+        private readonly AppDbContext _context;
+
+        public ManagementService(AppDbContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public async Task<List<QCReasons>> LoadQCReasons(string ic)
+        {
+            var qcReasons = new List<QCReasons>();
+
+            try
+            {
+                await using var conn = new SqlConnection(_context.Database.GetConnectionString());
+                await conn.OpenAsync();
+
+                await using var cmd = new SqlCommand(
+                    "SELECT DISTINCT qc_reason2 FROM vw_quality_control WHERE profile = @ic ORDER BY qc_reason", conn);
+                cmd.Parameters.Add("@ic", SqlDbType.VarChar).Value = ic;
+
+                await using var rdr = await cmd.ExecuteReaderAsync();
+                while (await rdr.ReadAsync())
+                {
+                    qcReasons.Add(new QCReasons { qc_reason = rdr["qc_reason"].ToString() });
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, "Error loading QCReasons for IC: {IC}", ic);
+            }
+
+            return qcReasons;
+        }
+
+        public async Task<List<EgrantsUsers>> LoadSpecialists(string ic)
+        {
+            var list = new List<EgrantsUsers>();
+
+            try
+            {
+                await using var conn = new SqlConnection(_context.Database.GetConnectionString());
+                await conn.OpenAsync();
+
+                await using var cmd = new SqlCommand(
+                    @"SELECT person_name, person_id 
+                      FROM vw_people 
+                      WHERE ic = @IC 
+                        AND application_type = 'egrants' 
+                        AND position_id > 1 
+                        AND PATINDEX('%,%', person_name) > 0 
+                      ORDER BY person_name", conn);
+
+                cmd.Parameters.Add("@IC", SqlDbType.VarChar).Value = ic;
+
+                await using var rdr = await cmd.ExecuteReaderAsync();
+                while (await rdr.ReadAsync())
+                {
+                    list.Add(new EgrantsUsers
+                    {
+                        PersonId = rdr["person_id"].ToString(),
+                        person_name = rdr["person_name"].ToString()
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, "Error loading specialists for IC: {IC}", ic);
+            }
+
+            return list;
+        }
+
+        public async Task<List<QCPersons>> LoadQCPersons(string ic)
+        {
+            var qcPersons = new List<QCPersons>();
+
+            try
+            {
+                await using var conn = new SqlConnection(_context.Database.GetConnectionString());
+                await conn.OpenAsync();
+
+                await using var cmd = new SqlCommand(
+                    "SELECT qc_reason, userid, person_id, person_name FROM vw_quality_control WHERE profile=@ic ORDER BY qc_reason",
+                    conn);
+
+                cmd.Parameters.Add("@ic", SqlDbType.VarChar).Value = ic;
+
+                await using var rdr = await cmd.ExecuteReaderAsync();
+                while (await rdr.ReadAsync())
+                {
+                    qcPersons.Add(new QCPersons
+                    {
+                        qc_reason = rdr["qc_reason"].ToString(),
+                        userid = rdr["userid"].ToString(),
+                        person_id = rdr["person_id"].ToString(),
+                        person_name = rdr["person_name"].ToString()
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, "Error loading QCPersons for IC: {IC}", ic);
+            }
+
+            return qcPersons;
+        }
+
+        public async Task<List<QCReports>> LoadQCReport(string ic)
+        {
+            var qcReports = new List<QCReports>();
+
+            try
+            {
+                await using var conn = new SqlConnection(_context.Database.GetConnectionString());
+                await conn.OpenAsync();
+
+                await using var cmd = new SqlCommand(
+                    @"WITH qc AS (
+                        SELECT COUNT(*) AS files_to_qc, 
+                               AVG(DATEDIFF(D, qc_date, GETDATE())) AS qc_days,
+                               qc_person_id 
+                        FROM egrants 
+                        WHERE qc_date IS NOT NULL
+                          AND qc_person_id IS NOT NULL
+                          AND qc_reason IS NOT NULL
+                          AND disabled_date IS NULL
+                          AND ic = @ic
+                          AND parent_id IS NULL
+                          AND grant_id IS NOT NULL
+                        GROUP BY qc_person_id
+                    )
+                    SELECT qc.files_to_qc, qc.qc_days,
+                           qc.qc_person_id, 
+                           COALESCE(vp.person_name, CAST(qc.qc_person_id AS VARCHAR(10))) AS qc_person_name
+                    FROM qc 
+                    INNER JOIN vw_people vp ON qc.qc_person_id = vp.person_id", conn);
+
+                cmd.Parameters.Add("@ic", SqlDbType.VarChar).Value = ic;
+
+                await using var rdr = await cmd.ExecuteReaderAsync();
+                while (await rdr.ReadAsync())
+                {
+                    qcReports.Add(new QCReports
+                    {
+                        files_to_qc = rdr["files_to_qc"].ToString(),
+                        qc_person_id = rdr["qc_person_id"].ToString(),
+                        qc_person_name = rdr["qc_person_name"].ToString(),
+                        qc_days = rdr["qc_days"].ToString()
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, "Error loading QCReport for IC: {IC}", ic);
+            }
+
+            return qcReports;
+        }
+
+        public async Task run_db(string act, int qcPersonId, string qcReason, int percent, int personId, string ic, string userId)
+        {
+            try
+            {
+                await using var conn = new SqlConnection(_context.Database.GetConnectionString());
+                await conn.OpenAsync();
+
+                await using var cmd = new SqlCommand("sp_web_management_qc_assign", conn)
+                {
+                    CommandType = CommandType.StoredProcedure
+                };
+
+                cmd.Parameters.Add("@act", SqlDbType.VarChar).Value = act;
+                cmd.Parameters.Add("@person_id", SqlDbType.Int).Value = personId;
+                cmd.Parameters.Add("@qc_person_id", SqlDbType.Int).Value = qcPersonId;
+                cmd.Parameters.Add("@qc_reason", SqlDbType.VarChar).Value = qcReason;
+                cmd.Parameters.Add("@percent", SqlDbType.Int).Value = percent;
+                cmd.Parameters.Add("@ic", SqlDbType.VarChar).Value = ic;
+                cmd.Parameters.Add("@operator", SqlDbType.VarChar).Value = userId;
+
+                await using var rdr = await cmd.ExecuteReaderAsync();
+                // No need to process results, just ensure execution
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, "Error executing RunDbAsync with act: {Act}, qcPersonId: {QcPersonId}, ic: {IC}, userId: {UserId}", act, qcPersonId, ic, userId);
+                throw; // rethrow if you want upstream handling
+            }
+        }
+    }
+}
