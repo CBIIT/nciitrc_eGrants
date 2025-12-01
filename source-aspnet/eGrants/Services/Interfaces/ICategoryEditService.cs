4051dc07540b37e168ce26f4557196aa2022088c
aalyaanferoz <163891259+aalyaanferoz@users.noreply.github.com>
2025-12-01 14:23:56 -0500

    Inital changes for CategoryEditPage to work

---
+++ source-aspnet/eGrants/Services/Interfaces/ICategoryEditService.cs
@@ -0,0 +1,29 @@
+﻿using eGrants.Models;
+
+namespace eGrants.Services.Interfaces
+{
+    public interface ICategoryEditService
+    {
+        /// <summary>
+        /// The load common categroies.
+        /// </summary>
+        /// <param name="ic">
+        /// The ic.
+        /// </param>
+        /// <returns>
+        /// The <see cref="List"/>.
+        /// </returns>
+        public List<Categories> LoadCommonCategories(string ic);
+
+        /// <summary>
+        /// The load local categroies.
+        /// </summary>
+        /// <param name="ic">
+        /// The ic.
+        /// </param>
+        /// <returns>
+        /// The <see cref="List"/>.
+        /// </returns>
+        public List<Categories> LoadLocalCategories(string ic);
+    }
+}
