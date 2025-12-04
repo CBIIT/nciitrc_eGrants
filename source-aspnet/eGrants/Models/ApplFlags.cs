4eb370b39eaa37979c13bc8dcc670845e2fd8494
aalyaanferoz <163891259+aalyaanferoz@users.noreply.github.com>
2025-12-04 18:15:28 -0500

    work for flag maintenace index and setup page, plus fixing one error

---
+++ source-aspnet/eGrants/Models/ApplFlags.cs
@@ -0,0 +1,30 @@
+﻿namespace eGrants.Models
+{
+    public class ApplFlags
+    {
+        /// <summary>
+        ///     Gets or sets the appl_id.
+        /// </summary>
+        public int appl_id { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the fgn.
+        /// </summary>
+        public string fgn { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the creator.
+        /// </summary>
+        public string creator { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the created_date.
+        /// </summary>
+        public string created_date { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the exclusion_reason.
+        /// </summary>
+        public string exclusion_reason { get; set; }
+    }
+}
---
+++ source-aspnet/eGrants/Services/Interfaces/IFlagMaintenanceService.cs
@@ -0,0 +1,115 @@
+﻿using eGrants.Models;
+
+namespace eGrants.Services.Interfaces
+{
+    public interface IFlagMaintenanceService
+    {
+        /// <summary>
+        ///     The load flag types.
+        /// </summary>
+        /// <returns>
+        ///     The <see cref="System.Collections.Generic.List`1" /> .
+        /// </returns>
+        public List<FlagTypes> LoadFlagTypes();
+
+        // load flags
+        /// <summary>
+        /// The load flags.
+        /// </summary>
+        /// <param name="act">
+        /// The act.
+        /// </param>
+        /// <param name="flag_type">
+        /// The flag_type.
+        /// </param>
+        /// <param name="admin_code">
+        /// The admin_code.
+        /// </param>
+        /// <param name="serial_num">
+        /// The serial_num.
+        /// </param>
+        /// <param name="id_string">
+        /// The id_string.
+        /// </param>
+        /// <param name="ic">
+        /// The ic.
+        /// </param>
+        /// <param name="userid">
+        /// The userid.
+        /// </param>
+        /// <returns>
+        /// The <see cref="System.Collections.Generic.List`1"/> .
+        /// </returns>
+        public List<Flags> LoadFlags(
+            string act,
+            string flag_type,
+            string admin_code,
+            int serial_num,
+            string id_string,
+            string ic,
+            string userid);
+
+        // add, delete or edit flag
+        /// <summary>
+        /// The run_db.
+        /// </summary>
+        /// <param name="act">
+        /// The act.
+        /// </param>
+        /// <param name="flag_type">
+        /// The flag_type.
+        /// </param>
+        /// <param name="admin_code">
+        /// The admin_code.
+        /// </param>
+        /// <param name="serial_num">
+        /// The serial_num.
+        /// </param>
+        /// <param name="id_string">
+        /// The id_string.
+        /// </param>
+        /// <param name="ic">
+        /// The ic.
+        /// </param>
+        /// <param name="userid">
+        /// The userid.
+        /// </param>
+        public void run_db(string act, string flag_type, string admin_code, int serial_num, string id_string, string ic, string userid);
+
+        /// <summary>
+        /// The load appls.
+        /// </summary>
+        /// <param name="act">
+        /// The act.
+        /// </param>
+        /// <param name="flag_type">
+        /// The flag_type.
+        /// </param>
+        /// <param name="admin_code">
+        /// The admin_code.
+        /// </param>
+        /// <param name="serial_num">
+        /// The serial_num.
+        /// </param>
+        /// <param name="id_string">
+        /// The id_string.
+        /// </param>
+        /// <param name="ic">
+        /// The ic.
+        /// </param>
+        /// <param name="userid">
+        /// The userid.
+        /// </param>
+        /// <returns>
+        /// The <see cref="System.Collections.Generic.List`1"/> .
+        /// </returns>
+        public List<ApplFlags> LoadAppls(
+            string act,
+            string flag_type,
+            string admin_code,
+            int serial_num,
+            string id_string,
+            string ic,
+            string userid);
+    }
+}
