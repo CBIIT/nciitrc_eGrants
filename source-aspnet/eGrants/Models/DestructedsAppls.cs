a57f81f706ebcbf23e6b2445a4eff8bff8188810
aalyaanferoz <163891259+aalyaanferoz@users.noreply.github.com>
2025-12-05 16:12:13 -0500

    all changes for grants archival process

---
+++ source-aspnet/eGrants/Models/DestructedsAppls.cs
@@ -0,0 +1,43 @@
+﻿namespace eGrants.Models
+{
+    /// <summary>
+    /// The destructeds appls.
+    /// </summary>
+    public class DestructedsAppls
+    {
+        /// <summary>
+        /// Gets or sets the appl_id.
+        /// </summary>
+        public string appl_id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the full_grant_num.
+        /// </summary>
+        public string full_grant_num { get; set; }
+
+        /// <summary>
+        /// Gets or sets the serial_num.
+        /// </summary>
+        public string serial_num { get; set; }
+
+        /// <summary>
+        /// Gets or sets the exception_code.
+        /// </summary>
+        public string exception_code { get; set; }
+
+        /// <summary>
+        /// Gets or sets the status_code.
+        /// </summary>
+        public string status_code { get; set; }
+
+        /// <summary>
+        /// Gets or sets the step_code.
+        /// </summary>
+        public string step_code { get; set; }
+
+        /// <summary>
+        /// Gets or sets the appl_editable.
+        /// </summary>
+        public string appl_editable { get; set; }
+    }
+}
