0c92a58ea50622d8f2e10cff44c10758c7c3ab07
aalyaanferoz <163891259+aalyaanferoz@users.noreply.github.com>
2025-12-05 11:18:20 -0500

    Adding logging

--- source-aspnet/eGrants/Services/FlagMaintenanceService.cs
+++ source-aspnet/eGrants/Services/FlagMaintenanceService.cs
@@ -7,11 +7,12 @@ using eGrants.Services.Interfaces;
 using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 
+using Serilog;
+
 namespace eGrants.Services
 {
     public class FlagMaintenanceService : IFlagMaintenanceService
     {
-        
         private readonly ISessionInfoService _sessionInfoService;
         private readonly AppDbContext _context;
 
@@ -29,29 +30,41 @@ namespace eGrants.Services
         /// </returns>
         public List<FlagTypes> LoadFlagTypes()
         {
-            using (var conn = new SqlConnection(_context.Database.GetConnectionString()))
+            try
             {
-                var cmd = new SqlCommand(
-               "SELECT UPPER(flag_type_code) as flag_type_code, flag_application_code FROM Grants_Flag_Master WHERE end_date is null",
-               conn);
 
-                cmd.CommandType = CommandType.Text;
+                using (var conn = new SqlConnection(_context.Database.GetConnectionString()))
+                {
+                    var cmd = new SqlCommand(
+                   "SELECT UPPER(flag_type_code) as flag_type_code, flag_application_code FROM Grants_Flag_Master WHERE end_date is null",
+                   conn);
 
-                conn.Open();
+                    cmd.CommandType = CommandType.Text;
 
-                var list = new List<FlagTypes>();
-                var rdr = cmd.ExecuteReader();
+                    conn.Open();
 
-                while (rdr.Read())
-                    list.Add(
-                        new FlagTypes { flag_type = rdr["flag_type_code"]?.ToString(), flag_application = rdr["flag_application_code"]?.ToString() });
+                    var list = new List<FlagTypes>();
+                    var rdr = cmd.ExecuteReader();
 
-                rdr.Close();
-                conn.Close();
+                    while (rdr.Read())
+                        list.Add(
+                            new FlagTypes
+                            {
+                                flag_type = rdr["flag_type_code"]?.ToString(),
+                                flag_application = rdr["flag_application_code"]?.ToString()
+                            });
 
-                return list;
+                    rdr.Close();
+                    conn.Close();
+
+                    return list;
+                }
             }
-               
+            catch (Exception ex)
+            {
+                Log.Error(ex, "LoadFlagTypes: Unexpected error occurred while loading flag types");
+                throw;
+            }
         }
 
         public List<Flags> LoadFlags(
@@ -63,43 +76,54 @@ namespace eGrants.Services
             string ic,
             string userid)
         {
-            using (var conn = new SqlConnection(_context.Database.GetConnectionString()))
+            try
             {
-                var cmd = new SqlCommand("dbo.sp_web_admin_flag_maintenance", conn);
-                cmd.CommandType = CommandType.StoredProcedure;
-                cmd.Parameters.Add("@act", SqlDbType.VarChar).Value = act;
-                cmd.Parameters.Add("@flag_type", SqlDbType.VarChar).Value = flag_type;
-                cmd.Parameters.Add("@admin_code", SqlDbType.VarChar).Value = admin_code;
-                cmd.Parameters.Add("@serial_num", SqlDbType.Int).Value = serial_num;
-                cmd.Parameters.Add("@id_string", SqlDbType.VarChar).Value = id_string;
-                cmd.Parameters.Add("@ic", SqlDbType.VarChar).Value = ic;
-                cmd.Parameters.Add("@operator", SqlDbType.VarChar).Value = userid;
 
-                conn.Open();
+                using (var conn = new SqlConnection(_context.Database.GetConnectionString()))
+                {
+                    var cmd = new SqlCommand("dbo.sp_web_admin_flag_maintenance", conn);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.Add("@act", SqlDbType.VarChar).Value = act;
+                    cmd.Parameters.Add("@flag_type", SqlDbType.VarChar).Value = flag_type;
+                    cmd.Parameters.Add("@admin_code", SqlDbType.VarChar).Value = admin_code;
+                    cmd.Parameters.Add("@serial_num", SqlDbType.Int).Value = serial_num;
+                    cmd.Parameters.Add("@id_string", SqlDbType.VarChar).Value = id_string;
+                    cmd.Parameters.Add("@ic", SqlDbType.VarChar).Value = ic;
+                    cmd.Parameters.Add("@operator", SqlDbType.VarChar).Value = userid;
 
-                var list = new List<Flags>();
-                var rdr = cmd.ExecuteReader();
+                    conn.Open();
 
-                while (rdr.Read())
-                    list.Add(
-                        new Flags
-                        {
-                            gf_id = rdr["gf_id"]?.ToString(),
-                            serial_num = rdr["serial_num"]?.ToString(),
-                            grant_id = rdr["grant_id"]?.ToString(),
-                            appl_id = rdr["appl_id"]?.ToString(),
-                            grant_num = rdr["grant_num"]?.ToString(),
-                            full_grant_num = rdr["full_grant_num"]?.ToString(),
-                            flag = rdr["flag"]?.ToString(),
-                            flag_type = rdr["flag_type"]?.ToString(),
-                            flag_application = rdr["flag_application"]?.ToString(),
-                            flag_icon_namepath = rdr["flag_icon_namepath"]?.ToString()
-                        });
+                    var list = new List<Flags>();
+                    var rdr = cmd.ExecuteReader();
 
-                rdr.Close();
-                conn.Close();
+                    while (rdr.Read())
+                        list.Add(
+                            new Flags
+                            {
+                                gf_id = rdr["gf_id"]?.ToString(),
+                                serial_num = rdr["serial_num"]?.ToString(),
+                                grant_id = rdr["grant_id"]?.ToString(),
+                                appl_id = rdr["appl_id"]?.ToString(),
+                                grant_num = rdr["grant_num"]?.ToString(),
+                                full_grant_num = rdr["full_grant_num"]?.ToString(),
+                                flag = rdr["flag"]?.ToString(),
+                                flag_type = rdr["flag_type"]?.ToString(),
+                                flag_application = rdr["flag_application"]?.ToString(),
+                                flag_icon_namepath = rdr["flag_icon_namepath"]?.ToString()
+                            });
 
-                return list;
+                    rdr.Close();
+                    conn.Close();
+
+                    return list;
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex,
+                    "LoadFlags: Unexpected error occurred - Act: {Act}, FlagType: {FlagType}, UserId: {UserId}",
+                    act, flag_type, userid);
+                throw;
             }
         }
 
@@ -113,65 +137,83 @@ namespace eGrants.Services
             string ic,
             string userid)
         {
-            using (var conn = new SqlConnection(_context.Database.GetConnectionString()))
+            try
             {
-                var cmd = new SqlCommand("dbo.sp_web_admin_flag_maintenance", conn);
-                cmd.CommandType = CommandType.StoredProcedure;
-                cmd.Parameters.Add("@act", SqlDbType.VarChar).Value = act;
-                cmd.Parameters.Add("@flag_type", SqlDbType.VarChar).Value = flag_type;
-                cmd.Parameters.Add("@admin_code", SqlDbType.VarChar).Value = admin_code;
-                cmd.Parameters.Add("@serial_num", SqlDbType.Int).Value = serial_num;
-                cmd.Parameters.Add("@id_string", SqlDbType.VarChar).Value = id_string;
-                cmd.Parameters.Add("@ic", SqlDbType.VarChar).Value = ic;
-                cmd.Parameters.Add("@operator", SqlDbType.VarChar).Value = userid;
 
-                conn.Open();
+                using (var conn = new SqlConnection(_context.Database.GetConnectionString()))
+                {
+                    var cmd = new SqlCommand("dbo.sp_web_admin_flag_maintenance", conn);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.Add("@act", SqlDbType.VarChar).Value = act;
+                    cmd.Parameters.Add("@flag_type", SqlDbType.VarChar).Value = flag_type;
+                    cmd.Parameters.Add("@admin_code", SqlDbType.VarChar).Value = admin_code;
+                    cmd.Parameters.Add("@serial_num", SqlDbType.Int).Value = serial_num;
+                    cmd.Parameters.Add("@id_string", SqlDbType.VarChar).Value = id_string;
+                    cmd.Parameters.Add("@ic", SqlDbType.VarChar).Value = ic;
+                    cmd.Parameters.Add("@operator", SqlDbType.VarChar).Value = userid;
 
-                var list = new List<ApplFlags>();
-                var rdr = cmd.ExecuteReader();
+                    conn.Open();
 
-                while (rdr.Read())
-                    list.Add(
-                        new ApplFlags
-                        {
-                            appl_id = Convert.ToInt32(rdr["appl_id"]),
-                            fgn = rdr["fgn"]?.ToString(),
-                            creator = rdr["creator"]?.ToString(),
-                            created_date = rdr["created_date"]?.ToString(),
-                            exclusion_reason = rdr["exclusion_reason"]?.ToString()
-                        });
+                    var list = new List<ApplFlags>();
+                    var rdr = cmd.ExecuteReader();
 
-                rdr.Close();
-                conn.Close();
+                    while (rdr.Read())
+                        list.Add(
+                            new ApplFlags
+                            {
+                                appl_id = Convert.ToInt32(rdr["appl_id"]),
+                                fgn = rdr["fgn"]?.ToString(),
+                                creator = rdr["creator"]?.ToString(),
+                                created_date = rdr["created_date"]?.ToString(),
+                                exclusion_reason = rdr["exclusion_reason"]?.ToString()
+                            });
 
-                return list;
+                    rdr.Close();
+                    conn.Close();
+
+                    return list;
+                }
             }
-           
-            
+            catch (Exception ex)
+            {
+                Log.Error(ex,
+                    "LoadAppls: Unexpected error occurred - Act: {Act}, FlagType: {FlagType}, UserId: {UserId}",
+                    act, flag_type, userid);
+                throw;
+            }
         }
 
         // add, delete or edit flag
         public void run_db(string act, string flag_type, string admin_code, int serial_num, string id_string, string ic, string userid)
         {
-            using (var conn = new SqlConnection(_context.Database.GetConnectionString()))
+            try
             {
-                var cmd = new SqlCommand("dbo.sp_web_admin_flag_maintenance", conn);
-                cmd.CommandType = CommandType.StoredProcedure;
-                cmd.Parameters.Add("@act", SqlDbType.VarChar).Value = act;
-                cmd.Parameters.Add("@flag_type", SqlDbType.VarChar).Value = flag_type;
-                cmd.Parameters.Add("@admin_code", SqlDbType.VarChar).Value = admin_code;
-                cmd.Parameters.Add("@serial_num", SqlDbType.Int).Value = serial_num;
-                cmd.Parameters.Add("@id_string", SqlDbType.VarChar).Value = id_string;
-                cmd.Parameters.Add("@ic", SqlDbType.VarChar).Value = ic;
-                cmd.Parameters.Add("@operator", SqlDbType.VarChar).Value = userid;
-                conn.Open();
-                var rdr = cmd.ExecuteReader();
-                rdr.Close();
-                conn.Close();
-            }                
-            
-        }
 
+                using (var conn = new SqlConnection(_context.Database.GetConnectionString()))
+                {
+                    var cmd = new SqlCommand("dbo.sp_web_admin_flag_maintenance", conn);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.Add("@act", SqlDbType.VarChar).Value = act;
+                    cmd.Parameters.Add("@flag_type", SqlDbType.VarChar).Value = flag_type;
+                    cmd.Parameters.Add("@admin_code", SqlDbType.VarChar).Value = admin_code;
+                    cmd.Parameters.Add("@serial_num", SqlDbType.Int).Value = serial_num;
+                    cmd.Parameters.Add("@id_string", SqlDbType.VarChar).Value = id_string;
+                    cmd.Parameters.Add("@ic", SqlDbType.VarChar).Value = ic;
+                    cmd.Parameters.Add("@operator", SqlDbType.VarChar).Value = userid;
+                    conn.Open();
+                    var rdr = cmd.ExecuteReader();
+                    rdr.Close();
+                    conn.Close();
+                 
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex,
+                    "run_db: Unexpected error occurred - Act: {Act}, FlagType: {FlagType}, UserId: {UserId}",
+                    act, flag_type, userid);
+                throw;
+            }
+        }
     }
 }
-
