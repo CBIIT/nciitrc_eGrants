91e47b8192cac9c35bec1bd755e1f8ff37bfaf3f
aalyaanferoz <163891259+aalyaanferoz@users.noreply.github.com>
2025-12-12 17:43:51 -0500

    Changes for closeout notification download (THIS DOES NOT WORK IN LEGACY SYSTEM)

--- source-aspnet/eGrants/Services/SessionInfoService.cs
+++ source-aspnet/eGrants/Services/SessionInfoService.cs
@@ -20,7 +20,8 @@ namespace eGrants.Services
                 EgrantsDocModifyRelativePath = session.TryGetValue("EgrantsDocModifyRelativePath", out var ModifyrelativePathBytes) && ModifyrelativePathBytes != null ? System.Text.Encoding.UTF8.GetString(ModifyrelativePathBytes) : "",
                 EgrantsDocEmail = session.TryGetValue("EgrantsDocEmail", out var EgrantsDocEmailBytes) && EgrantsDocEmailBytes != null ? System.Text.Encoding.UTF8.GetString(EgrantsDocEmailBytes) : "",
                 CertPath = session.TryGetValue("certPath", out var certPathBytes) && certPathBytes != null ? System.Text.Encoding.UTF8.GetString(certPathBytes) : "",
-                CertPass = session.TryGetValue("certPass", out var certPassBytes) && certPassBytes != null ? System.Text.Encoding.UTF8.GetString(certPassBytes) : ""
+                CertPass = session.TryGetValue("certPass", out var certPassBytes) && certPassBytes != null ? System.Text.Encoding.UTF8.GetString(certPassBytes) : "",
+                EraUrlBase = session.TryGetValue("eraUrlBase", out var eraUrlBaseBytes) && eraUrlBaseBytes != null ? System.Text.Encoding.UTF8.GetString(eraUrlBaseBytes) : ""
             };
         }
     }
