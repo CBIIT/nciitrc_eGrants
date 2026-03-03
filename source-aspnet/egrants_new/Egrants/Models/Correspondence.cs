318a71332b62c73649967dd1fb8f90a10507b73c
aalyaanferoz <163891259+aalyaanferoz@users.noreply.github.com>
2026-03-03 12:39:32 -0500

    changes for converting era service to REST API

---
+++ source-aspnet/egrants_new/Egrants/Models/Correspondence.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+
+namespace egrants_new.Models
+{
+    public class Correspondence
+    {
+        [JsonProperty("notificationName")]
+        public string NotificationName { get; set; }
+
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("fromAddress")]
+        public string FromAddress { get; set; }
+
+        [JsonProperty("toAddress")]
+        public string ToAddress { get; set; }
+
+        [JsonProperty("ccAddress")]
+        public string CcAddress { get; set; }
+
+        [JsonProperty("subject")]
+        public string Subject { get; set; }
+
+        [JsonProperty("sentDate")]
+        public string SentDate { get; set; }
+
+        [JsonProperty("emailContent")]
+        public string EmailContent { get; set; }
+    }
+}
---
+++ source-aspnet/egrants_new/Egrants/Models/GrantCorrespondenceRequest.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace egrants_new.Models
+{
+    public class GrantCorrespondenceRequest
+    {
+        [JsonProperty("applId")]
+        public string ApplId { get; set; }
+    }
+}
