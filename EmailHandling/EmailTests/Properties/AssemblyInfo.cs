03f6739a9bb19143fcac6d4f0a53a6380aea4ed9
dehuffdc <daryl.dehuff@nih.gov>
2026-05-08 14:25:02 -0400

    Store final changes for step 'Upgrade EmailTests\EmailTests.csproj'

--- EmailHandling/EmailTests/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-[assembly: ComVisible(false)]
-
-[assembly: Guid("cf293a9d-d8c9-47f0-9484-49e45d067dfd")]
