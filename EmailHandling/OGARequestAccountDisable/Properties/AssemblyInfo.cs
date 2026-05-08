eb2f8570ed7c0de8505a04621004ee03856c15f3
dehuffdc <daryl.dehuff@nih.gov>
2026-05-08 10:46:45 -0400

    Move assembly metadata from AssemblyInfo.cs to .csproj
    Assembly metadata attributes such as title, company, product, copyright, and version information were removed from AssemblyInfo.cs and added to the OGARequestAccountDisable.csproj file. This centralizes assembly information in the project file, aligning with modern .NET project conventions. No functional code changes were made.

--- EmailHandling/OGARequestAccountDisable/Properties/AssemblyInfo.cs
+++ EmailHandling/OGARequestAccountDisable/Properties/AssemblyInfo.cs
@@ -1,16 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("OGARequestAccountDisable")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("National Cancer Institute")]
-[assembly: AssemblyProduct("OGARequestAccountDisable")]
-[assembly: AssemblyCopyright("Copyright © National Cancer Institute 2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -21,16 +11,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("f8dede06-fbeb-44eb-a2fd-e450bd0e06f4")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
