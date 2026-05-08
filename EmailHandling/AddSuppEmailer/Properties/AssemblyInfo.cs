fccf6e8dcd5aaf5dfd2e9cdf38973b02f9231d0b
dehuffdc <daryl.dehuff@nih.gov>
2026-05-08 11:25:12 -0400

    Store final changes for step 'Upgrade AddSuppEmailer\AddSuppEmailer.csproj'

--- EmailHandling/AddSuppEmailer/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-using System.Reflection;
-using System.Runtime.InteropServices;
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-[assembly: ComVisible(false)]
-[assembly: Guid("a1b2c3d4-1111-2222-3333-444455556666")]
